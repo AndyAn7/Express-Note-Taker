efc8681a48ffa7fbc0acbc168529d2fbdc41f26f
Andrew An <andyan7@gmail.com>
2022-03-07 21:03:32 -0800

    Added get route and get requests

--- server.js
+++ server.js
@@ -8,3 +8,17 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+const noteS = require('./db/db.json');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, /public/notes.html));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(noteS)
+});
