f4d603dd18c4138fcb0b4a727e86d2af45a252b1
Andrew An <andyan7@gmail.com>
2022-03-08 21:30:40 -0800

    express delete fs write and port

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, /public/notes.html));
 });
 
-app.get('/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
@@ -24,14 +24,14 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    let fPath = path.join(__dirname, 'db/db.json');
+    let fPath = path.join(__dirname, './db/db.json');
     noteN = req.body;
 
     noteN.id = uuidv4();
 
     noteS.push(noteN);
 
-    fs.writeFile(fPath. JSON.stringify(noteS), (err) => {
+    fs.writeFile(fPath.JSON.stringify(noteS), (err) => {
         if (err) {
             return console.log(err);
         } else {
@@ -41,3 +41,30 @@ app.post('/api/notes', (req, res) => {
 
     res.json(noteN);
 });
+
+app.delete('/api/notes/:id', function(req, res) {
+    let id = req.params.id.toString();
+
+    for (i = 0; i < noteS.length; i++) {
+        if (noteS[i].id == id) {
+            res.send(noteS[i]);
+            noteS.splice(i, 1);
+            break;
+    }
+};
+
+let fPath = path.join(__dirname, './db/db.json');
+fs.writeFile(fPath, JSON.stringify(noteS), (err) => {
+    if (err) {
+        return console.log(err);
+    } else {
+        console.log('Deleted');
+    }
+})});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(PORT, () =>
+console.log(`Listening at http://localhost:${PORT}`));
