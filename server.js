06bf66755877943ef2890ee7c6d3aa740598fb41
Andrew An <andyan7@gmail.com>
2022-03-08 21:49:41 -0800

    Correction to file name

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({extended:true}));
 const noteS = require('./db/db.json');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, /public/notes.html));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
